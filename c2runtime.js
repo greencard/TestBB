b4f69af51b35bd18d722e48c94a0bace29b3de37
Sergio Korenko <sergio-korenko@yandex.ru>
2013-07-15 22:29:09 +0300

    Test #2

--- c2runtime.js
+++ c2runtime.js
@@ -11723,8 +11723,8 @@ cr.getProjectModel = function() { return [
 	[
 	[
 		"Layout 1",
-		480,
-		360,
+		1024,
+		768,
 		false,
 		"Event sheet 1",
 		1950560624317476,
@@ -11745,7 +11745,7 @@ cr.getProjectModel = function() { return [
 			0,
 			[
 			[
-				[245, 97, 0, 270, 64, 0, 0, 1, 0.5, 0.5, 0, 0, []],
+				[527, 440, 0, 270, 64, 0, 0, 1, 0.5, 0.5, 0, 0, []],
 				0,
 				0,
 				[
@@ -11777,8 +11777,8 @@ cr.getProjectModel = function() { return [
 	],
 	"media/",
 	false,
-	480,
-	360,
+	1024,
+	768,
 	4,
 	true,
 	true,
